d0eb99dca2eab80cd037e6821cba8dffd362495b
timothy_ding <timothy_ding@hotmail.com>
2021-01-20 10:35:55 +0800

    Initial commit

--- src/main/java/com/sim/application/classes/TechniqueMap.java
+++ src/main/java/com/sim/application/classes/TechniqueMap.java
@@ -3,6 +3,7 @@ package com.sim.application.classes;
 import com.sim.application.obfuscation.Technique;
 import java.util.LinkedHashMap;
 
+@SuppressWarnings("serial")
 public class TechniqueMap extends LinkedHashMap<String, Technique> {
 
     public Technique put(Technique value) {
