30afdfc01abe7cc55ea91d6c8ec519c6973a869b
timothy_ding <timothy_ding@hotmail.com>
2021-03-20 10:38:48 +0800

    refactor: Changed InputDisplay and OutputDisplay to extend from CodeDisplay to eliminate duplicate code

--- src/main/java/com/sim/application/views/components/InputDisplay.java
+++ src/main/java/com/sim/application/views/components/InputDisplay.java
@@ -34,136 +34,14 @@ import org.fxmisc.wellbehaved.event.InputMap;
 import org.fxmisc.wellbehaved.event.Nodes;
 import org.reactfx.collection.ListModification;
 
-public class InputDisplay extends VBox implements Initializable, IInputDisplay {
+public class InputDisplay extends CodeDisplay implements IInputDisplay {
 
-    @FXML
-    private Label label;
-    @FXML
-    private CodeArea code;
-    @FXML
-    private Button button;
-
-    public InputDisplay() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource(
-                "com/sim/application/views/components/CodeDisplay.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-
-        try {
-            fxmlLoader.load();
-
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
-
-    @Override
-    public void setCode(String code) {
-        this.code.replaceText(code);
-    }
+    public InputDisplay() {}
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
         DisplayUploadedCodeController.initialize(this);
 
-        code.setParagraphGraphicFactory(LineNumberFactory.get(code));
-        code.getVisibleParagraphs().addModificationObserver
-        (
-            new VisibleParagraphStyler<>( code, this::computeHighlighting )
-        );
-        // auto-indent: insert previous line's indents on enter
-        final Pattern whiteSpace = Pattern.compile( "^\\s+" );
-        code.addEventHandler( KeyEvent.KEY_PRESSED, KE ->
-        {
-            if ( KE.getCode() == KeyCode.ENTER ) {
-                int caretPosition = code.getCaretPosition();
-                int currentParagraph = code.getCurrentParagraph();
-                Matcher m0 = whiteSpace.matcher( code.getParagraph( currentParagraph ).getSegments().get( 0 ) );
-
-                if ( m0.find() ) Platform.runLater( () -> code.insertText( caretPosition + 1, m0.group() ) );
-            }
-        });
-
-        // Tab indentation set to 4 spaces
-        InputMap<KeyEvent> im = InputMap.consume(
-                EventPattern.keyPressed(KeyCode.TAB),
-                e -> code.replaceSelection("    ")
-        );
-        Nodes.addInputMap(code, im);
     }
-
-    private StyleSpans<Collection<String>> computeHighlighting(String text) {
-        Matcher matcher = StringUtil.PATTERN.matcher(text);
-        int lastKwEnd = 0;
-        StyleSpansBuilder<Collection<String>> spansBuilder
-                = new StyleSpansBuilder<>();
-        while(matcher.find()) {
-            String styleClass =
-                    matcher.group("KEYWORD") != null ? "keyword" :
-                            matcher.group("PAREN") != null ? "paren" :
-                            matcher.group("BRACE") != null ? "brace" :
-                            matcher.group("BRACKET") != null ? "bracket" :
-                            matcher.group("SEMICOLON") != null ? "semicolon" :
-                            matcher.group("STRING") != null ? "string" :
-                            matcher.group("COMMENT") != null ? "comment" :
-                            null; /* never happens */ assert styleClass != null;
-            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
-            lastKwEnd = matcher.end();
-        }
-        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
-        return spansBuilder.create();
-    }
-
-    private class VisibleParagraphStyler<PS, SEG, S> implements Consumer<ListModification<? extends Paragraph<PS, SEG, S>>>
-    {
-        private final GenericStyledArea<PS, SEG, S> area;
-        private final Function<String,StyleSpans<S>> computeStyles;
-        private int prevParagraph, prevTextLength;
-
-        public VisibleParagraphStyler( GenericStyledArea<PS, SEG, S> area, Function<String,StyleSpans<S>> computeStyles )
-        {
-            this.computeStyles = computeStyles;
-            this.area = area;
-        }
-
-        @Override
-        public void accept( ListModification<? extends Paragraph<PS, SEG, S>> lm )
-        {
-            if ( lm.getAddedSize() > 0 )
-            {
-                int paragraph = Math.min( area.firstVisibleParToAllParIndex() + lm.getFrom(), area.getParagraphs().size()-1 );
-                String text = area.getText( paragraph, 0, paragraph, area.getParagraphLength( paragraph ) );
-
-                if ( paragraph != prevParagraph || text.length() != prevTextLength )
-                {
-                    int startPos = area.getAbsolutePosition( paragraph, 0 );
-                    Platform.runLater( () -> area.setStyleSpans( startPos, computeStyles.apply( text ) ) );
-                    prevTextLength = text.length();
-                    prevParagraph = paragraph;
-                }
-            }
-        }
-    }
-
-    public final String getLabel() {
-        return label.textProperty().get();
-    }
-
-    public final void setLabel(String text) {
-        label.textProperty().set(text);
-    }
-
-    public final StringProperty labelProperty() { return label.textProperty(); }
-
-    public final String getButtonText() {
-        return button.textProperty().get();
-    }
-
-    public final void setButtonText(String text) {
-        button.textProperty().set(text);
-        button.setVisible(true);
-    }
-
-    public final StringProperty buttonTextProperty() { return button.textProperty(); }
 }
