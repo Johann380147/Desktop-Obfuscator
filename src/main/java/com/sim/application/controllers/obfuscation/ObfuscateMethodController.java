0b86436ffadb51cf5b32ec8e34fb2d4043b2cf9a
timothy_ding <timothy_ding@hotmail.com>
2021-05-02 03:01:31 +0800

    feat: Added Method obfuscation

--- src/main/java/com/sim/application/controllers/obfuscation/ObfuscateMethodController.java
+++ src/main/java/com/sim/application/controllers/obfuscation/ObfuscateMethodController.java
@@ -1,19 +1,39 @@
 package com.sim.application.controllers.obfuscation;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.resolution.MethodUsage;
 import com.google.common.collect.BiMap;
 import com.sim.application.classes.ClassMap;
 import com.sim.application.classes.JavaFile;
 import com.sim.application.classes.Problem;
 import com.sim.application.techniques.FailedTechniqueException;
 import com.sim.application.techniques.Technique;
+import com.sim.application.utils.StringUtil;
+import javafx.util.Pair;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public final class ObfuscateMethodController extends Technique {
     private static ObfuscateMethodController instance;
     private final String name = "Method Obfuscation";
-    private final String description = "Adds excessive overloading and splits method implementation where possible";
+    private final String description = "Splits method implementation where possible";
 
     public static ObfuscateMethodController getInstance() {
         if (instance == null) {
@@ -36,6 +56,205 @@ public final class ObfuscateMethodController extends Technique {
 
     @Override
     public void execute(BiMap<JavaFile, CompilationUnit> sourceFiles, ClassMap classMap, List<Problem> problemList) throws FailedTechniqueException {
+        for (var file : sourceFiles.keySet()) {
+            var currFile = file.getFileName();
 
+            var unit = sourceFiles.get(file);
+            var usedNames = getAllUsedNames(unit);
+            var changeList = new ArrayList<Pair<Node, MethodDeclaration>>();
+
+            try {
+                var methodVisitor = new MethodVisitor(usedNames, changeList);
+                methodVisitor.visit(unit, null);
+
+                for (var change : changeList) {
+                    var parentNode = change.getKey();
+                    var method = change.getValue();
+
+                    if (parentNode instanceof ClassOrInterfaceDeclaration) {
+                        var clazz = (ClassOrInterfaceDeclaration)parentNode;
+                        clazz.getMembers().add(method);
+                    } else if (parentNode instanceof ObjectCreationExpr) {
+                        var clazz = (ObjectCreationExpr)parentNode;
+                        clazz.addAnonymousClassBody(method);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new FailedTechniqueException(currFile + " failed to obfuscate. " + e.getMessage()).setFileName(currFile);
+            }
+        }
+    }
+
+    private HashSet<String> getAllUsedNames(CompilationUnit unit) {
+        var methodNames = new HashSet<String>();
+        unit.getTypes().forEach(typeDeclaration -> {
+            if (!(typeDeclaration instanceof AnnotationDeclaration)) {
+                var resolvedType = typeDeclaration.resolve();
+                var methods = resolvedType.getAllMethods();
+                methodNames.addAll(methods.stream().map(MethodUsage::getName).collect(Collectors.toSet()));
+            }
+        });
+        return methodNames;
+    }
+
+    private class MethodVisitor extends ModifierVisitor<Void> {
+
+        private final HashSet<String> usedNames;
+        private final ArrayList<Pair<Node, MethodDeclaration>> changeList;
+
+        private MethodVisitor(HashSet<String> usedNames,
+                              ArrayList<Pair<Node, MethodDeclaration>> changeList) {
+            this.usedNames = usedNames;
+            this.changeList = changeList;
+        }
+
+        public MethodDeclaration visit(MethodDeclaration md, Void args) {
+            super.visit(md, args);
+
+            md.getBody().ifPresent(body -> {
+                var clazz = getContainingClass(md);
+                if (clazz.isEmpty()) return;
+                var statements = body.getStatements();
+                if (statements.size() < 2) return;
+                int indexToCut = statements.size() / 2;
+
+                // Collect all variable declarations from statements below the cutoff index
+                var newMethodParameters = new NodeList<Parameter>();
+                newMethodParameters.addAll(md.getParameters());
+                AtomicBoolean hasErrors = new AtomicBoolean(false);
+                for (int i = 0; i < indexToCut; i++) {
+                    var statement = statements.get(i);
+                    statement.ifExpressionStmt(expressionStmt -> {
+                        var expression = expressionStmt.getExpression();
+                        expression.ifVariableDeclarationExpr(variableDeclarationExpr ->
+                                variableDeclarationExpr.getVariables().forEach(variableDeclarator -> {
+                                    String type = getType(variableDeclarator);
+                                    if (type == null) {
+                                        hasErrors.set(true);
+                                        return;
+                                    }
+                                    var parameter = new Parameter();
+                                    parameter.setType(type);
+                                    parameter.setName(variableDeclarator.getNameAsString());
+                                    newMethodParameters.add(parameter);
+                                })
+                        );
+                    });
+                }
+
+                if (hasErrors.get()) return;
+
+                // Store all statements beyond the cutoff index
+                var newMethodStatements = new NodeList<Statement>();
+                for (int i = indexToCut; i < statements.size(); i++) {
+                    newMethodStatements.add(statements.get(i));
+                }
+                // Remove statements from old method
+                statements.removeAll(newMethodStatements);
+
+                // Generate new method with the old method's statements
+                var newMethod = new MethodDeclaration();
+                var name = generateMethodName();
+                newMethod.setName(name);
+                newMethod.setModifiers(md.getModifiers());
+                newMethod.setTypeParameters(md.getTypeParameters());
+                var hasReturnStmt = newMethodStatements.stream().anyMatch(Statement::isReturnStmt);
+                newMethod.setType(hasReturnStmt ? md.getTypeAsString() : "void");
+                newMethod.setParameters(newMethodParameters);
+                newMethod.setThrownExceptions(md.getThrownExceptions());
+                var receiverParameter = md.getReceiverParameter();
+                receiverParameter.ifPresent(newMethod::setReceiverParameter);
+                newMethod.setBody(new BlockStmt(newMethodStatements));
+
+                // Add method call to new method in old method
+                var methodCall = new MethodCallExpr();
+                methodCall.setName(name);
+                newMethodParameters.forEach(parameter -> methodCall.addArgument(parameter.getNameAsString()));
+                if (!md.getType().isVoidType() && hasReturnStmt) {
+                    body.addStatement(new ReturnStmt(methodCall));
+                } else {
+                    body.addStatement(new ExpressionStmt(methodCall));
+                }
+
+                // Add new method later to class
+                changeList.add(new Pair<>(clazz.get(), newMethod));
+            });
+
+            return md;
+        }
+
+        private Optional<? extends Node> getContainingClass(MethodDeclaration md) {
+            Node clazz = null;
+            var parentNode = md.getParentNode();
+            if (parentNode.isPresent() && parentNode.get() instanceof ObjectCreationExpr) {
+                clazz = parentNode.get();
+            } else {
+                var containingClass = md.findAncestor(ClassOrInterfaceDeclaration.class);
+                if (containingClass.isPresent()) {
+                    clazz = containingClass.get();
+                }
+            }
+            if (clazz == null) {
+                return Optional.empty();
+            } else {
+                return Optional.of(clazz);
+            }
+        }
+
+        private String getType(VariableDeclarator variableDeclarator) {
+            AtomicReference<String> type = new AtomicReference<>(variableDeclarator.getTypeAsString());
+            if (variableDeclarator.getInitializer().isPresent()) {
+                if (variableDeclarator.getTypeAsString().equals("var")) {
+                    var initializer = variableDeclarator.getInitializer().get();
+                    try {
+                        var resolvedType = initializer.calculateResolvedType();
+                        type.set(resolvedType.describe());
+                    } catch (Exception e) {
+                        return null;
+                    }
+                }
+            } else {
+                if (variableDeclarator.getTypeAsString().equals("var")) {
+                    return null;
+                } else if (variableDeclarator.getType().isPrimitiveType()) {
+                    String value = "";
+                    if (variableDeclarator.getTypeAsString().equals("char")) value = "'\\0'";
+                    else if (variableDeclarator.getTypeAsString().equals("int")) value = String.valueOf(getPrimitiveDefaultValue(int.class));
+                    else if (variableDeclarator.getTypeAsString().equals("double")) value = String.valueOf(getPrimitiveDefaultValue(double.class));
+                    else if (variableDeclarator.getTypeAsString().equals("long")) value = String.valueOf(getPrimitiveDefaultValue(long.class)) + "L";
+                    else if (variableDeclarator.getTypeAsString().equals("boolean")) value = String.valueOf(getPrimitiveDefaultValue(boolean.class));
+
+                    if (value.equals("")) {
+                        return null;
+                    }
+                    variableDeclarator.setInitializer(value);
+                } else {
+                    variableDeclarator.setInitializer("null");
+                }
+            }
+            return type.get();
+        }
+
+        private String generateMethodName() {
+            var randomWord = StringUtil.randomWord();
+            if (randomWord != null) {
+                while (usedNames.contains(randomWord)) {
+                    randomWord = StringUtil.randomWord();
+                }
+            } else {
+                randomWord = StringUtil.randomString(20, true);
+                while (usedNames.contains(randomWord)) {
+                    randomWord = StringUtil.randomString(20, true);
+                }
+            }
+            usedNames.add(randomWord);
+            return randomWord;
+        }
+
+        private <T> T getPrimitiveDefaultValue(Class<T> type) {
+            if (type.isPrimitive()) return (T)Array.get(Array.newInstance(type, 1), 0);
+            else return null;
+        }
     }
 }
--- src/main/java/com/sim/application/utils/StringUtil.java
+++ src/main/java/com/sim/application/utils/StringUtil.java
@@ -1,12 +1,23 @@
 package com.sim.application.utils;
 
+import com.sim.application.controllers.obfuscation.ObfuscateArtController;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.regex.Pattern;
 
 public class StringUtil {
+    private static String[] dictionary;
     private static String ALPHANUMERIC_CHARACTERS =
             "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -61,6 +72,43 @@ public class StringUtil {
         if (Arrays.stream(KEYWORDS).anyMatch(name::equals)) return randomString(maxLength, randomLength);
 
         return str;
+    }
+
+    public static String randomWord() {
+        if (dictionary == null) {
+            var result = initDictionary();
+            if (result == false) return null;
+        }
+        UniformRandomProvider rng = RandomSource.create(RandomSource.MT);
+        var word = dictionary[rng.nextInt(dictionary.length)];
+
+        var index = rng.nextInt(3);
+        switch (index) {
+            case 0:
+                break;
+            case 1: word = "get" + capitalize(word);
+                break;
+            case 2: word = "set" + capitalize(word);
+                break;
+        }
+        return word;
+    }
+
+    public static String capitalize(String word) {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
+    private static boolean initDictionary() {
+        InputStream stream = ObfuscateArtController.class.getClassLoader()
+                .getResourceAsStream("text/dictionary.txt");
+        if (stream == null) return false;
+        try {
+            var words = IOUtils.toString(stream, StandardCharsets.UTF_8);
+            dictionary = words.split("\\r?\\n");
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public static boolean isInteger(String input) {
